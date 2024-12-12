f95380a9fd89f2cd3e10d9de466775ef5416330c
saraz06 <sarazach.de@gmail.com>
2024-12-12 13:31:56 +0100

    Test

--- src/Projektarbeit/PersonalisiertesBuch.java
+++ src/Projektarbeit/PersonalisiertesBuch.java
@@ -1,4 +1,38 @@
 package Projektarbeit;
 
+import javax.swing.*;
+
 public class PersonalisiertesBuch {
+    private JTextField textFieldFür;
+    private JTextField textFieldGravur;
+    private JRadioButton rBHard;
+    private JRadioButton rBSoft;
+    private JTextField textFieldCh1Name;
+    private JTextField textFieldCh2Name;
+    private JTextField textFieldCh1Alter;
+    private JTextField textField6;
+    private JComboBox comboBoxCh1Geschlecht;
+    private JComboBox comboBox2;
+    private JRadioButton radioButtonMagisch1;
+    private JRadioButton radioButtonMagisch2;
+    private JLabel JLabelName;
+    private JLabel JlabelCover;
+    private JLabel JLabelFür;
+    private JLabel JLabelGravur;
+    private JLabel JLabelCh1;
+    private JLabel JLabelCh2;
+    private JLabel JLabelCh1Name;
+    private JLabel JLabelCh2Name;
+    private JLabel JLabelCh1Alter;
+    private JLabel JLabelCh2Alter;
+    private JLabel JLabelCh1Geschlecht;
+    private JLabel JLabelStoryline;
+    private JComboBox comboBoxStoryline;
+    private JButton buttonBestellen;
+    private JLabel JLabelGeschenk;
+    private JTextField textFieldLesezeichen;
+    private JTextField textFieldPreis;
+    private JTextField textFieldWrenkorb;
+    private JButton buttonPreis;
+    private JButton buttonWarenkorb;
 }
